54c7a38599c4edfff019831c508195ee97c5695c
felix-ong <felixongweicong@gmail.com>
2022-10-23 22:52:55 +0800

    Update mqtt.py

--- yolov7/mqtt.py
+++ yolov7/mqtt.py
@@ -2,8 +2,29 @@ import paho.mqtt.client as mqtt
 import cv2
 import os
 import time
+import datetime
+import json
+from collections import defaultdict
+
+import firebase_admin
+from firebase_admin import firestore
+from firebase_admin import credentials
+
+cred = credentials.Certificate('cs3237-fridge-firebase-adminsdk-d14fo-88295eb35b.json')
+app = firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+doc_ref = db.collection(str('stocks')).document(str(time.time()))
+doc_ref.set({
+    'bottles': 0,
+    'bananas': 0,
+    'apples': 0,
+    'timestamp': time.time(),
+})
+
+
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
@@ -21,13 +42,35 @@ def on_message(client, userdata, msg):
         if result:
             # Run command line
             os.system("python detect.py --weights yolov7-e6e.pt --conf 0.5 --img-size 1280  --source test.jpg --no-trace >> out.txt")
-            # TODO
+            dict = defaultdict(lambda x: 0)
             with open("counts.txt", "r") as f:
-                lines = f.readlines()
+                lines = f.read().splitlines()
                 for line in lines:
-                    name, count = line.split(" ")
-                    print(f"{count} {name}s")
-            # Send time, item name, count to database, publish to fridge/stock
+                    print(line.split(" "))
+                    name, count = line.rsplit(" ", 1)
+                    dict[name] = count
+
+            # send to firebase
+            doc_ref = db.collection(str('stocks')).document(str(time.time()))
+            doc_ref.set({
+                'bottles': 0,
+                'bananas': 0,
+                'apples': 0,
+                'timestamp': time.time(),
+            })
+
+            THRESHOLD = 2  # TODO: get this value from firebase/predicted
+            sensor_val = ""
+            for item in ['bananas', 'apples', 'bottles']:
+                count = dict[item] if item in dict else 0
+                if not count:  # count == 0
+                    sensor_val = "r"
+                elif count < THRESHOLD:
+                    sensor_val = "y"
+                else:
+                    sensor_val = "g"
+
+            client.publish("fridge/stock", sensor_val)
     else:
         print("Fridge door is closed.")
     
